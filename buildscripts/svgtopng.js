585dd665ef7b7ecb37b2c4267fe0d75c37452a79
Robert Nordan <robert.nordan@norkart.no>
2015-01-27 09:06:44 +0100

    Create CSS to fallback to PNG for ye olde IE

--- buildscripts/svgtopng.js
+++ buildscripts/svgtopng.js
@@ -5,7 +5,3 @@ var path = require('path');
 svg2png(path.resolve('src/images/toggle.svg'), path.resolve('src/images/toggle.png'), 1.0, function (err) {
 	ncp('src/images/toggle.png', 'dist/images/toggle.png');
 });
-
-svg2png(path.resolve('src/images/toggle.svg'), path.resolve('src/images/toggle-2x.png'), 2.0, function (err) {
-	ncp('src/images/toggle-2x.png', 'dist/images/toggle-2x.png');
-});
