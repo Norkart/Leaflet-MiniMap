b4acfeab73917d8ff9aa4a71968cb454a711f55d
Denjell <denjell@das-gaengeviertel.info>
2015-07-22 15:40:42 +0200

    CSS fixes - Restructured src folder to match new dist - Restructured dist folder with new build scripts - Restructured example folder with new build scripts - Fixed .min.css to provide ../images/toggle.svg instead of ../dist/images/toggle.svg - removed unneccesary 'px' from eg. top: 0px; → top: 0; - patched all example html files

---
+++ buildscripts/examples.js
@@ -0,0 +1,7 @@
+var ncp = require('ncp');
+var path = require('path');
+
+    ncp('dist/css/Control.MiniMap.min.css', 'example/css/Control.MiniMap.min.css');
+    ncp('dist/images/toggle.png', 'example/images/toggle.png');
+    ncp('dist/images/toggle.svg', 'example/images/toggle.svg');
+    ncp('dist/js/Control.MiniMap.min.js', 'example/js/Control.MiniMap.min.js');
