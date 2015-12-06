f45342d2a17a200cca56cb51a7bf964996330ec6
Machiste Quintana <mnquintana@users.noreply.github.com>
2015-12-06 09:53:13 -0500

    Add linting with happiness

--- src/Control.MiniMap.js
+++ src/Control.MiniMap.js
@@ -1,4 +1,4 @@
-// Following https://github.com/Leaflet/Leaflet/blob/master/PLUGIN-GUIDE.md
+// Following https:// github.com/Leaflet/Leaflet/blob/master/PLUGIN-GUIDE.md
 (function (factory, window) {
 
 	// define an AMD module that relies on 'leaflet'
@@ -32,15 +32,15 @@
 			height: 150,
 			collapsedWidth: 19,
 			collapsedHeight: 19,
-			aimingRectOptions: {color: "#ff7800", weight: 1, clickable: false},
-			shadowRectOptions: {color: "#000000", weight: 1, clickable: false, opacity:0, fillOpacity:0},
+			aimingRectOptions: {color: '#ff7800', weight: 1, clickable: false},
+			shadowRectOptions: {color: '#000000', weight: 1, clickable: false, opacity: 0, fillOpacity: 0},
 			strings: {hideText: 'Hide MiniMap', showText: 'Show MiniMap'}
 		},
 
-		//layer is the map layer to be shown in the minimap
+		// layer is the map layer to be shown in the minimap
 		initialize: function (layer, options) {
 			L.Util.setOptions(this, options);
-			//Make sure the aiming rects are non-clickable even if the user tries to set them clickable (most likely by forgetting to specify them false)
+			// Make sure the aiming rects are non-clickable even if the user tries to set them clickable (most likely by forgetting to specify them false)
 			this.options.aimingRectOptions.clickable = false;
 			this.options.shadowRectOptions.clickable = false;
 			this._layer = layer;
@@ -50,7 +50,7 @@
 
 			this._mainMap = map;
 
-			//Creating the container and stopping events from spilling through to the main map.
+			// Creating the container and stopping events from spilling through to the main map.
 			this._container = L.DomUtil.create('div', 'leaflet-control-minimap');
 			this._container.style.width = this.options.width + 'px';
 			this._container.style.height = this.options.height + 'px';
@@ -73,11 +73,11 @@
 
 			this._miniMap.addLayer(this._layer);
 
-			//These bools are used to prevent infinite loops of the two maps notifying each other that they've moved.
+			// These bools are used to prevent infinite loops of the two maps notifying each other that they've moved.
 			this._mainMapMoving = false;
 			this._miniMapMoving = false;
 
-			//Keep a record of this to prevent auto toggling when the user explicitly doesn't want it.
+			// Keep a record of this to prevent auto toggling when the user explicitly doesn't want it.
 			this._userToggledDisplay = false;
 			this._minimized = false;
 
@@ -123,8 +123,8 @@
 
 		_addToggleButton: function () {
 			this._toggleDisplayButton = this.options.toggleDisplay ? this._createButton(
-				'', this.options.strings.hideText, ('leaflet-control-minimap-toggle-display leaflet-control-minimap-toggle-display-'
-				+ this.options.position), this._container, this._toggleDisplayButtonClicked, this) : undefined;
+				'', this.options.strings.hideText, ('leaflet-control-minimap-toggle-display leaflet-control-minimap-toggle-display-' +
+				this.options.position), this._container, this._toggleDisplayButtonClicked, this) : undefined;
 
 			this._toggleDisplayButton.style.width = this.options.collapsedWidth + 'px';
 			this._toggleDisplayButton.style.height = this.options.collapsedHeight + 'px';
@@ -160,7 +160,7 @@
 		},
 
 		_setDisplay: function (minimize) {
-			if (minimize != this._minimized) {
+			if (minimize !== this._minimized) {
 				if (!this._minimized) {
 					this._minimize();
 				} else {
@@ -210,74 +210,73 @@
 			this._aimingRect.setBounds(this._mainMap.getBounds());
 		},
 
-		_onMiniMapMoveStarted:function (e) {
+		_onMiniMapMoveStarted: function (e) {
 			if (!this.options.centerFixed) {
 				var lastAimingRect = this._aimingRect.getBounds();
 				var sw = this._miniMap.latLngToContainerPoint(lastAimingRect.getSouthWest());
 				var ne = this._miniMap.latLngToContainerPoint(lastAimingRect.getNorthEast());
-				this._lastAimingRectPosition = {sw:sw,ne:ne};
+				this._lastAimingRectPosition = {sw: sw, ne: ne};
 			}
 		},
 
 		_onMiniMapMoving: function (e) {
 			if (!this.options.centerFixed) {
 				if (!this._mainMapMoving && this._lastAimingRectPosition) {
-					this._shadowRect.setBounds(new L.LatLngBounds(this._miniMap.containerPointToLatLng(this._lastAimingRectPosition.sw),this._miniMap.containerPointToLatLng(this._lastAimingRectPosition.ne)));
-					this._shadowRect.setStyle({opacity:1,fillOpacity:0.3});
+					this._shadowRect.setBounds(new L.LatLngBounds(this._miniMap.containerPointToLatLng(this._lastAimingRectPosition.sw), this._miniMap.containerPointToLatLng(this._lastAimingRectPosition.ne)));
+					this._shadowRect.setStyle({opacity: 1, fillOpacity: 0.3});
 				}
 			}
 		},
 
 		_onMiniMapMoved: function (e) {
 			if (!this._mainMapMoving) {
-				var center = this.options.centerFixed || this._mainMap.getCenter();
-
 				this._miniMapMoving = true;
 				this._mainMap.setView(this._mainMap.getCenter(), this._decideZoom(false));
-				this._shadowRect.setStyle({opacity:0,fillOpacity:0});
+				this._shadowRect.setStyle({opacity: 0, fillOpacity: 0});
 			} else {
 				this._mainMapMoving = false;
 			}
 		},
 
-		_isZoomLevelFixed:function() {
+		_isZoomLevelFixed: function () {
 			var zoomLevelFixed = this.options.zoomLevelFixed;
 			return this._isDefined(zoomLevelFixed) && this._isInteger(zoomLevelFixed);
 		},
 
 		_decideZoom: function (fromMaintoMini) {
-			if (!this._isZoomLevelFixed()){
-				if (fromMaintoMini)
+			if (!this._isZoomLevelFixed()) {
+				if (fromMaintoMini) {
 					return this._mainMap.getZoom() + this.options.zoomLevelOffset;
-				else {
+				} else {
 					var currentDiff = this._miniMap.getZoom() - this._mainMap.getZoom();
 					var proposedZoom = this._miniMap.getZoom() - this.options.zoomLevelOffset;
 					var toRet;
 
 					if (currentDiff > this.options.zoomLevelOffset && this._mainMap.getZoom() < this._miniMap.getMinZoom() - this.options.zoomLevelOffset) {
-						//This means the miniMap is zoomed out to the minimum zoom level and can't zoom any more.
+						// This means the miniMap is zoomed out to the minimum zoom level and can't zoom any more.
 						if (this._miniMap.getZoom() > this._lastMiniMapZoom) {
-							//This means the user is trying to zoom in by using the minimap, zoom the main map.
+							// This means the user is trying to zoom in by using the minimap, zoom the main map.
 							toRet = this._mainMap.getZoom() + 1;
-							//Also we cheat and zoom the minimap out again to keep it visually consistent.
-							this._miniMap.setZoom(this._miniMap.getZoom() -1);
+							// Also we cheat and zoom the minimap out again to keep it visually consistent.
+							this._miniMap.setZoom(this._miniMap.getZoom() - 1);
 						} else {
-							//Either the user is trying to zoom out past the mini map's min zoom or has just panned using it, we can't tell the difference.
-							//Therefore, we ignore it!
+							// Either the user is trying to zoom out past the mini map's min zoom or has just panned using it, we can't tell the difference.
+							// Therefore, we ignore it!
 							toRet = this._mainMap.getZoom();
 						}
 					} else {
-						//This is what happens in the majority of cases, and always if you configure the min levels + offset in a sane fashion.
+						// This is what happens in the majority of cases, and always if you configure the min levels + offset in a sane fashion.
 						toRet = proposedZoom;
 					}
 					this._lastMiniMapZoom = this._miniMap.getZoom();
 					return toRet;
 				}
 			} else {
-				if (fromMaintoMini)
+				if (fromMaintoMini) {
 					return this.options.zoomLevelFixed;
-				else
+				} else {
 					return this._mainMap.getZoom();
+				}
 			}
 		},
 
@@ -296,13 +295,13 @@
 			return this._minimized;
 		},
 
-		_isInteger:function(value) {
+		_isInteger: function (value) {
 			return typeof value === 'number';
 		},
 
-		_isDefined:function(value) {
+		_isDefined: function (value) {
 			return typeof value !== 'undefined';
-		},
+		}
 	});
 
 	L.Map.mergeOptions({
